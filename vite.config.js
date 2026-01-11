f259549da4539d4ca297e8125c1b5c58603b90dc
sadiqce <sadiqce85@yahoo.com>
2026-01-11 10:30:06 -0800

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nexiomaisolutions/", // REPLACE 'nexiom-ai' WITH YOUR REPO NAME
+  base: "nexiomaisolutions.com/", // For custom domain at root
 })
