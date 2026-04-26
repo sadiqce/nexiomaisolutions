9798cc288fc4ea59b1497bbfef37342dc7211872
sadiqce <sadiqce85@yahoo.com>
2026-04-26 16:23:42 -0700

    update gitignore

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Root path
+  base: "/nexiomaisolutions/", // GitHub Pages project base path
 })
